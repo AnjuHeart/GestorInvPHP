8715c62048dff2bfda77d21cd9c2cf2ca2f514ce
Yhunn <jybrant.efren@gmail.com>
2022-06-03 20:35:41 -0600

    añadido restaurante

--- agregarsalida.php
+++ agregarsalida.php
@@ -1,15 +1,17 @@
 <?php
+    SESSION_START();
     $idVenta = $_POST['id'];
     $idProducto = $_POST['idprod'];
     $cantVenta = $_POST['cant'];
     $totalVenta = $_POST['total'];
+    $idrestaurante = $_SESSION['idrestaurante'];
 
     $con = mysqli_connect('localhost','root','',"gestor_php");
     mysqli_select_db($con,'gestor_php');
     $sql="UPDATE `ventas` SET `estado` = 'entregada' WHERE `ventas`.`id` = $idVenta;";
     mysqli_query($con,$sql);
 
-    $sql="INSERT INTO `salidas`(`id`, `venta`, `producto`, `cantidad`, `total`, `fecha`)";
-    $sql=$sql."VALUES (DEFAULT,".$idVenta.",".$idProducto.",".$cantVenta.",".$totalVenta.",now())";
+    $sql="INSERT INTO `salidas`(`id`, `venta`, `producto`, `cantidad`, `total`, `fecha`, `idrestaurante`)";
+    $sql=$sql."VALUES (DEFAULT,".$idVenta.",".$idProducto.",".$cantVenta.",".$totalVenta.",now(),".$idrestaurante.")";
     mysqli_query($con,$sql);
 ?>
